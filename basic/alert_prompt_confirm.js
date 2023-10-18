42f841116c84f7b211bcce75a212444cce05f861
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-10-19 00:15:57 +0900

    Create alert_prompt_confirm.js

---
+++ basic/alert_prompt_confirm.js
@@ -0,0 +1,7 @@
+// Question and answer
+const name = prompt("Name?", "You should know what your name is");
+alert(`Hello ${name}, you suka blyat`);
+
+// Confirming something, get either true or false
+const areYouAdult = confirm("You are now entering 18+ hot horny meme site. Are you an adult? No kids welcomed legally.");
+console.log(`Are you an adult? : ${areYouAdult}`);
