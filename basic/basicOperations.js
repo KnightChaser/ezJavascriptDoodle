50903e54d8c488efbc8244a620a258bd1b3a41ec
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-10-19 00:28:49 +0900

    Create basicOperations.js

---
+++ basic/basicOperations.js
@@ -0,0 +1,14 @@
+// Simple operation, very super duper ez ez!!
+
+let money = 12;
+money += 11;
+money += 1;
+
+console.log(`Balance: ${money} BTC`);			// 24
+
+money++;
+console.log(`Balance: ${money} BTC`);			// 25
+
+++money;
+console.log(`Balance: ${money} BTC`);			// 26
+console.log(`Balance: ${++money} BTC`);         // 27
